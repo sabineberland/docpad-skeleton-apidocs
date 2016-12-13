815fea10777c41fa99c384c78637772fefa2a991
Sabine <sabine.berland@mirakl.com>
2016-12-13 14:20:31 +0100

    Update chewieConfig.js

--- chewieConfig.js
+++ chewieConfig.js
@@ -4,7 +4,7 @@ const config = {
   skeletonDestination: './src',
   skeletonOutDestination: './out',
   placeholdersLocation: './src/raw/placeholders',
-  docuUrl: process.env.docuURL || 'http://your.webpage.io',
+  docuUrl: process.env.docuURL || 'https://sabineberland.github.io',
   notClonedRepositoriesFile: 'notClonedRepositories.json',
   indepenedentDocuRepositoriesFile: 'indepenedentDocuRepositories.json',
 
